3957989489a3eae09ad6a2f5c9070ff5a05a0670
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-10-01 17:36:58 +0300

    Add obstacle and move it.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,28 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rigidBody;
     public float jumpForce = 15f;
+    public float gravityModifier = 1f;
+    private bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        isOnGround = true;
     }
 }
